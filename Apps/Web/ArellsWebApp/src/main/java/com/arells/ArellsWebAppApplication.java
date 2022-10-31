ba6199aae05ab73e131ba82ba0e1189b3546eb22
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-10-30 17:27:09 -0700

    Added Tomcat Servlet

--- Apps/Web/ArellsWebApp/src/main/java/com/arells/ArellsWebAppApplication.java
+++ Apps/Web/ArellsWebApp/src/main/java/com/arells/ArellsWebAppApplication.java
@@ -1,7 +1,13 @@
 package com.arells;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+import com.arells.ArellsWebAppApplication;
 
 @SpringBootApplication
 public class ArellsWebAppApplication {
@@ -9,5 +15,17 @@ public class ArellsWebAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArellsWebAppApplication.class, args);
 	}
+	@Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9090);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        return tomcat;
+        }	
 
 }
