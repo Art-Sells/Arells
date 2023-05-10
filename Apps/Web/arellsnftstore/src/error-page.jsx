714192e5966884a946b0c624cacbd0387663def2
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-05-09 20:43:52 -0700

    Meta Errors resolved

--- Apps/Web/arellsnftstore/src/error-page.jsx
+++ Apps/Web/arellsnftstore/src/error-page.jsx
@@ -21,7 +21,7 @@ export default function ErrorPage() {
 		
 {/*<!-- Below information for social media sharing and search-engine/browser optimization -->*/}		
 		<meta name="title" content="Arells"/>
-		<meta name="description" content="Art Sells"/>
+		<meta name="description" content="Error Re-Router"/>
 		<meta name="google" content="nositelinkssearchbox"/>
 		<meta name="keywords" content="Arells"/>
 		<meta name="author" content="Arells"/>
@@ -31,24 +31,23 @@ export default function ErrorPage() {
 				{/*<!-- Change below link after test -->*/}
 		<link rel="canonical" href="https://arells.com"/>
 		
-		<meta property="og:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg">	
+		<meta property="og:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>	
 		<meta property="og:site_name" content="Arells"/>	
 		<meta property="og:type" content="object"/>				
 		<meta property="og:title" content="Arells"/>
 				{/*<!-- Change below link after test -->*/}
 		<meta propety="og:url" content="https://arells.com"/>
-		<meta property="og:description" content="Art Sells"/>
+		<meta property="og:description" content="Error Re-Router"/>
 		<meta property="og:image:type" content="image/jpg"/>
 		<meta property="og:image:width" content="700"/>
 		<meta property="og:image:height" content="400"/>
 		
-		<meta name="twitter:title" content="Art Sells">
+		<meta name="twitter:title" content="Arells"/>
 		<meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>
 				{/*<!-- Change below link after test -->*/}
 		<meta name="twitter:url" content="https://arells.com"/>
 		<meta name="twitter:card" content="summary_large_image"/>
-		<meta name="twitter:description" content="Arells"/>
-		<meta name="description" content="Arells"/>
+		<meta name="twitter:description" content="Error Re-Router"/>
 {/*<!-- Above information for social media sharing and search-engine/browser optimization -->*/}	
 
         <title>Oops!</title>
