7de11efbde795f79b2c95dcfe73fa1dd40b216ef
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-05-10 23:40:04 -0700

    Added Meta Home and CSS Artifacts

--- Apps/Web/arellsnftstore/src/Home.jsx
+++ Apps/Web/arellsnftstore/src/Home.jsx
@@ -7,9 +7,67 @@ import Favicon from "react-favicon";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <>
+        <Favicon url="favicon.ico"/>
+        <HelmetProvider>	
+                                                        
+        <meta name="robots" content="noimageindex"/>
+		
+        {/*<!-- Change below links after test -->*/}
+          <link rel="stylesheet" type="text/css" href="css/homers.css"/>	
+            
+          <meta charset="UTF-8"/>
+
+          {/*<!-- Below information for social media sharing and search-engine/browser optimization -->*/}		
+          <meta name="title" content="Arells"/>
+          <meta name="description" content="Art Sells"/>
+          <meta name="google" content="nositelinkssearchbox"/>
+          <meta name="keywords" content="Arells"/>
+          <meta name="author" content="Arells"/>
+          <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1" />
+
+              {/*<!-- Change below link after test -->*/}
+          <link rel="canonical" href="https://arells.com"/>
+
+          <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>	
+          <meta property="og:site_name" content="Arells"/>	
+          <meta property="og:type" content="object"/>				
+          <meta property="og:title" content="Arells"/>
+              {/*<!-- Change below link after test -->*/}
+          <meta propety="og:url" content="https://arells.com"/>
+          <meta property="og:description" content="Art Sells"/>
+          <meta property="og:image:type" content="image/jpg"/>
+          <meta property="og:image:width" content="700"/>
+          <meta property="og:image:height" content="400"/>
+
+          <meta name="twitter:title" content="Arells"/>
+          <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>
+              {/*<!-- Change below link after test -->*/}
+          <meta name="twitter:url" content="https://arells.com"/>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:description" content="Art Sells"/>
+        {/*<!-- Above information for social media sharing and search-engine/browser optimization -->*/}	
+        
+                <title>Arells</title>
+
+        {/* body styling element */}
+          <style>
+          {`
+          body {
+            text-align: center;
+            font-family: Arial;
+            margin-top: 0%;
+            margin-left: 0%;
+            margin-right: 0%;
+            margin-bottom: 0%;
+              background: white;
+            }
+          `}
+          </style>        
+
+
+        </HelmetProvider>      
+    </>
   );
 }
 
