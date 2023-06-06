23834dee75bd63ae627ce8ee3d52b0ca191d9489
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-06-05 19:26:32 -0700

    Upgraded Metadata Info

--- Apps/Web/arellsdigitalassets/src/app/page.js
+++ Apps/Web/arellsdigitalassets/src/app/page.js
@@ -3,56 +3,46 @@
 import './css/Home.css';
 
 import Link from 'next/link';
-import Head from 'next/head'
-
-import { Metadata } from 'next';
  
 export const metadata = {
   title: 'Arells',
+  metaTags: {
+    standard: {
+      robots: 'noimageindex',
+      charset: 'UTF-8',
+      'X-UA-Compatible': 'IE=edge',
+      title: 'Arells',
+      description: 'Art Sells',
+      google: 'nositelinkssearchbox',
+      keywords: 'Arells',
+      author: 'Arells',
+      viewport: 'width=device-width,user-scalable=yes,initial-scale=1',
+      canonical: 'https://arells.com'
+    },
+    openGraph: {
+      image: 'https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg',
+      site_name: 'Arells',
+      type: 'object',
+      title: 'Arells',
+      url: 'https://arells.com',
+      description: 'Art Sells',
+      'image:type': 'image/jpg',
+      'image:width': '700',
+      'image:height': '400'
+    },
+    twitter: {
+      title: 'Arells',
+      image: 'https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg',
+      url: 'https://arells.com',
+      card: 'summary_large_image',
+      description: 'Art Sells'
+    }
+  }
 };
 
 const Home = () => {
   return (
     <>
-    <Head>
-      <meta name="robots" content="noimageindex"/>
-
-      <meta charset="UTF-8"/>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>	
-
-      {/*<!-- Below information for social media sharing and search-engine/browser optimization -->*/}		
-      <meta name="title" content="Arells"/>
-      <meta name="description" content="Art Sells"/>
-      <meta name="google" content="nositelinkssearchbox"/>
-      <meta name="keywords" content="Arells"/>
-      <meta name="author" content="Arells"/>
-      <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1" />
-
-          {/*<!-- Change below link after test -->*/}
-      <link rel="canonical" href="https://arells.com"/>
-
-      <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>	
-      <meta property="og:site_name" content="Arells"/>	
-      <meta property="og:type" content="object"/>				
-      <meta property="og:title" content="Arells"/>
-          {/*<!-- Change below link after test -->*/}
-      <meta propety="og:url" content="https://arells.com"/>
-      <meta property="og:description" content="Art Sells"/>
-      <meta property="og:image:type" content="image/jpg"/>
-      <meta property="og:image:width" content="700"/>
-      <meta property="og:image:height" content="400"/>
-
-      <meta name="twitter:title" content="Arells"/>
-      <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>
-          {/*<!-- Change below link after test -->*/}
-      <meta name="twitter:url" content="https://arells.com"/>
-      <meta name="twitter:card" content="summary_large_image"/>
-      <meta name="twitter:description" content="Art Sells"/>
-      {/*<!-- Above information for social media sharing and search-engine/browser optimization -->*/}	
-
-      <title>Arells</title>          
-
-    </Head>
       <div id="overlayy">
   
         <img id="arells-iconn" src="/icons&images/Arells-Icon.png"/>
