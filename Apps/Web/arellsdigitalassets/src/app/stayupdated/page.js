1c20d3d0ccfa0b31afa3a87592c301e9bc1be7e5
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-06-17 15:40:18 -0700

    Rebuilding Components

--- Apps/Web/arellsdigitalassets/src/app/stayupdated/page.js
+++ Apps/Web/arellsdigitalassets/src/app/stayupdated/page.js
@@ -1,6 +1,6 @@
 import '../css/stayupdated.css';
-import {signUp} from '../../../scripts/stayupdated.js';
-
+import {RWmodal} from './RWmodal.js';
+import axios from 'axios'; 
 
 
 
@@ -46,12 +46,29 @@ const stayupdated = () => {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
   
-	const handleSubmit = () => {
-	  signUp(email, firstName, lastName, RWmodal);
-	  setEmail("");
-	  setFirstName("");
-	  setLastName("");
-	};	
+	const signUp = () => {
+	  if (email === "" || firstName === "" || lastName === "") {
+		RWmodal.open(1, 'ENTER INFORMATION');
+	  } else {
+		axios.post("https://api.apispreadsheets.com/data/uAv9KS8S9kojekky/", {
+		  email: email,
+		  first_name: firstName,
+		  last_name: lastName
+		}, {
+		  headers:{
+			accessKey: "c492c5cefcf9fdde44bbcd84a97465f1",
+			secretKey: "ac667f2902e4e472c82aff475a4a7a07"
+		  }
+		}).then(response => {
+		  // reset the input fields
+		  setEmail("");
+		  setFirstName("");
+		  setLastName("");
+		  // Open your modal here with message 'SUBMITTED'
+		  RWmodal.open(1, 'SUBMITTED');
+		});
+	  }
+	};
 	
     return (
         <>
