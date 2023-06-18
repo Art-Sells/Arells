b8e0c3f9d00dd6925b6fb11537e17b77b8dbf279
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-06-17 17:32:02 -0700

    Solving React Rendering issue

--- Apps/Web/arellsdigitalassets/src/app/stayupdated/page.js
+++ Apps/Web/arellsdigitalassets/src/app/stayupdated/page.js
@@ -1,41 +1,8 @@
+"use client";
+
+import Head from 'next/head'
 import '../css/stayupdated.css';
 import { useState } from 'react';
-
-export const metadata = {
-	title: 'Stay Updated',
-	metaTags: {
-	  standard: {
-		robots: 'noimageindex',
-		'X-UA-Compatible': 'IE=edge',
-		charset: 'UTF-8',
-		title: 'Stay Updated',
-		description: 'Stay updated on our development',
-		google: 'nositelinkssearchbox',
-		keywords: 'Arells',
-		author: 'Arells',
-		viewport: 'width=device-width,user-scalable=yes,initial-scale=1',
-		canonical: 'https://arells.com/stayupdated'
-	  },
-	  openGraph: {
-		image: 'https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg',
-		site_name: 'Arells',
-		type: 'website',
-		title: 'Stay Updated',
-		url: 'https://arells.com/stayupdated',
-		description: 'Stay updated on our development',
-		'image:type': 'image/jpg',
-		'image:width': '700',
-		'image:height': '400'
-	  },
-	  twitter: {
-		title: 'Stay Updated',
-		image: 'https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg',
-		url: 'https://arells.com/stayupdated',
-		card: 'summary_large_image',
-		description: 'Stay updated on our development'
-	  }
-	}
-  };
 
 const stayupdated = () => {
 
@@ -65,10 +32,50 @@ const stayupdated = () => {
 			setSubmitted(true);	
 		  }
 		}
-	  };
+	};
+
+	const closeEnterInformation = () => {
+		setEnterInformation(false);
+	};
+
+	const closeSubmitted = () => {
+		setSubmitted(false);
+	};
 	
     return (
         <>
+
+<Head>
+        <meta name="robots" content="noimageindex"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+        <meta charset="UTF-8"/>
+
+        <meta name="title" content="Stay Updated"/>
+        <meta name="description" content="Stay updated on our development"/>
+        <meta name="google" content="nositelinkssearchbox"/>
+        <meta name="keywords" content="Arells"/>
+        <meta name="author" content="Arells"/>
+        <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1" />
+
+        <link rel="canonical" href="https://arells.com/stayupdated"/>
+
+        <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
+        <meta property="og:site_name" content="Arells"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Stay Updated"/>
+        <meta property="og:url" content="https://arells.com/stayupdated"/>
+        <meta property="og:description" content="Stay updated on our development"/>
+        <meta property="og:image:type" content="image/jpg"/>
+        <meta property="og:image:width" content="700"/>
+        <meta property="og:image:height" content="400"/>
+
+        <meta name="twitter:title" content="Stay Updated"/>
+        <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
+        <meta name="twitter:url" content="https://arells.com/stayupdated"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:description" content="Stay updated on our development"/>
+      </Head>		
 
 		{showEnterInformation && (
 			<div id="enterInformation">
