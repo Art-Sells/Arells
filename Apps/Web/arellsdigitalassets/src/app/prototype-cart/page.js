13cbbb9f430bb4affb4d04b1f9b41a9635c121fc
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-06-28 21:58:24 -0700

    Temporary Fix for Prototype Word

--- Apps/Web/arellsdigitalassets/src/app/prototype-cart/page.js
+++ Apps/Web/arellsdigitalassets/src/app/prototype-cart/page.js
@@ -9,6 +9,15 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const prototypeCart = () => {
+	const [elementId, setElementId] = useState('');
+
+	useEffect(() => {
+		// Set the id prop here; this code will only execute on the client-side
+		setElementId('prototype-cart-word');
+	}, []);
+
+
+
 
 {/*<!-- useState constants below -->*/}
 	const [purchaseComplete, setPurchaseComplete] = useState(false);
@@ -335,6 +344,7 @@ const prototypeCart = () => {
 				</div>					
 
 			)}			
+				<p id={elementId}>PROTOTYPE</p>
 		</div>			
 		     
         </>
