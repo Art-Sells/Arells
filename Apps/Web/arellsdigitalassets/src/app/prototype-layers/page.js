91550c60db4484f23cc9a77fe7021507217b7770
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-08-25 19:22:15 -0700

    Done with Layer Image Restructuring

--- Apps/Web/arellsdigitalassets/src/app/prototype-layers/page.js
+++ Apps/Web/arellsdigitalassets/src/app/prototype-layers/page.js
@@ -10,6 +10,7 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Layers = () => {
 
@@ -240,7 +241,12 @@ const Layers = () => {
 					<p id="connect-wallet-words">CONNECT WALLET</p>
 					<button id="connectWallet"
 						onClick={walletConnected}>
-						<img id="wallet-icon" src="/icons&images/prototype/coinbase-wallet-logo.png"/>
+						<Image 
+						id="wallet-icon"
+						alt=""
+						width={50}
+						height={50}
+						src="/icons&images/prototype/coinbase-wallet-logo.png"/>
 					</button>		
 				</div>
 			</div>	
@@ -264,18 +270,33 @@ const Layers = () => {
 				{/*<!-- Change below link after test -->*/}
 					<Link legacyBehavior href="/">
 						<a id="icon-link-layers">
-							<img id="arells-icon-layers" src="/icons&images/prototype/Arells-Icon-Home.png"/>
+							<Image
+							alt=""
+							height={16}
+							width={15}
+							id="arells-icon-layers" 
+							src="/icons&images/prototype/Arells-Icon-Home.png"/>
 						</a>
 					</Link>	
 					{cartLinkLayers && (
 						<button id="cart-link-layers" onClick={connectWallet}>
-							<img id="cart-icon-layers" src="/icons&images/prototype/shopping-cart-empty.png"/>
+							<Image
+							alt=""
+							height={15}
+							width={16} 
+							id="cart-icon-layers" 
+							src="/icons&images/prototype/shopping-cart-empty.png"/>
 						</button>
 					)}	
 					{cartLinkConnectedLayers && (
 						<Link legacyBehavior href="/prototype-cart">
 							<a id="cart-link-connected-layers">
-								<img id="cart-icon-layers" src="/icons&images/prototype/shopping-cart-empty.png"/>
+								<Image
+								alt=""
+								height={15}
+								width={16} 
+								id="cart-icon-layers" 
+								src="/icons&images/prototype/shopping-cart-empty.png"/>
 							</a>	
 						</Link>
 					)}	
@@ -283,12 +304,22 @@ const Layers = () => {
 					{cartLinkFullLayers && (
 						<Link legacyBehavior href="/prototype-cart">
 							<a id="cart-link-full-layers">
-								<img id="cart-icon-full-layers" src="/icons&images/prototype/shopping-cart-full.png"/>
+								<Image
+								alt=""
+								height={15}
+								width={16} 
+								id="cart-icon-full-layers" 
+								src="/icons&images/prototype/shopping-cart-full.png"/>
 							</a>	
 						</Link>
 					)}	
 				</div>
-				<img id="word-logo-layers" src="/icons&images/Arells-Logo-Ebony.png"/>	
+				<Image
+				alt=""
+				width={110}  
+				height={35}
+				id="word-logo-layers" 
+				src="/icons&images/Arells-Logo-Ebony.png"/>	
 				<p id="slogan-layers">NEVER LOSE MONEY SELLING ART</p>
 				{walletConnectedDivLayers && (
 					<div id="wallet-connected-div-layers">
@@ -300,13 +331,23 @@ const Layers = () => {
 				)}
 
             <div id="layers">
-                <img id="photo-layers" src="/icons&images/prototype/4.jpg"/>
+                <Image
+				alt=""
+				width={400}  
+				height={400} 
+				id="photo-layers" 
+				src="/icons&images/prototype/4.jpg"/>
                 <h3 id="name-layers">Layers</h3>
                 <div id="share-div-layers">
                     <p id="share-div-desc-layers">SHARE</p> 
                     <button id="copy-link-layers"
                     onClick={copyLink}>
-                        <img id="copy-link-icon-layers" src="/icons&images/prototype/link.png"/>
+                        <Image
+						alt=""
+						width={15}  
+						height={8}
+						id="copy-link-icon-layers" 
+						src="/icons&images/prototype/link.png"/>
                         COPY LINK
                     </button>	
                 </div>
@@ -376,19 +417,34 @@ const Layers = () => {
 					<span>
 						<button id="fingerprints-button"
 							onClick={comingSoon}>
-							<img id="fingerprints-icon" src="/icons&images/prototype/etherscan-logo.png"/>
+							<Image
+							alt=""
+							width={25}  
+							height={25}
+							id="fingerprints-icon" 
+							src="/icons&images/prototype/etherscan-logo.png"/>
 						</button>	
 					</span>
 					<span>
 						<button id="fingerprints-button"
 							onClick={comingSoon}>
-							<img id="fingerprints-icon" src="/icons&images/prototype/ipfs.png"/>
+							<Image
+							alt=""
+							width={25}  
+							height={25}
+							id="fingerprints-icon" 
+							src="/icons&images/prototype/ipfs.png"/>
 						</button>	
 					</span>
 					<span>
 						<button id="fingerprints-button"
 							onClick={comingSoon}>
-							<img id="fingerprints-icon" src="/icons&images/prototype/ipfslite.png"/>
+							<Image
+							alt=""
+							width={25}  
+							height={23}
+							id="fingerprints-icon" 
+							src="/icons&images/prototype/ipfslite.png"/>
 						</button>	
 					</span>
 				</div>	    		
