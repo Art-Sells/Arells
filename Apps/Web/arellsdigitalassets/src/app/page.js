1cd8ffc588e0036dda5c68255bbb11841f9f0032
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-08-28 00:52:28 -0700

    Until Tomorrow

--- Apps/Web/arellsdigitalassets/src/app/page.js
+++ Apps/Web/arellsdigitalassets/src/app/page.js
@@ -1,48 +1,194 @@
-import Index from '../components/Index';
+"use client";
 
-const Home = () => {
+// Change below link after test
+import './css/Home.css';
+import './css/modals/copiedlink.css';
+
+//Loader Styles
+import './css/modals/loading/spinnerBackground.css';
+import styles from './css/modals/loading/spinner.module.css';
+
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+
+const Index = () => {
+
+
+  //Loader Functions
+  const [showLoading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState({
+    arellsIcon: false,
+    wordLogo: false,
+  });
+  const handleImageLoaded = (imageName) => {
+    setImagesLoaded(prevState => ({ 
+      ...prevState, 
+      [imageName]: true 
+    }));
+  };
+  useEffect(() => {
+    if (Object.values(imagesLoaded).every(Boolean)) {
+      setLoading(false);
+    }
+  }, [imagesLoaded]);
+
+
+
+  useEffect(() => {
+    function resetPrototype() {
+      sessionStorage.removeItem('walletConnectedSession'); 
+
+      sessionStorage.removeItem('blueOrangeAdded');
+      sessionStorage.removeItem('blueOrangePurchased');
+
+      sessionStorage.removeItem('beachHousesAdded');
+      sessionStorage.removeItem('beachHousesPurchased');
+
+      sessionStorage.removeItem('colourGlassAdded');
+      sessionStorage.removeItem('colourGlassPurchased');
+
+      sessionStorage.removeItem('layersAdded');
+      sessionStorage.removeItem('layersPurchased');
+
+      sessionStorage.removeItem('succinctDropAdded');
+      sessionStorage.removeItem('succinctDropPurchased');
+
+      sessionStorage.removeItem('paintRainAdded');
+      sessionStorage.removeItem('paintRainPurchased');
+    }
+    
+    resetPrototype();
+  }, []);
 
   return (
     <>
-      <Index/>
+    <Head>
+        <meta name="robots" content="noimageindex"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1" />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
+        <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
+    </Head>
+
+      {showLoading && (
+        <div id="spinnerBackground">
+          <Image 
+            alt="" 
+            width={29}
+            height={30}
+            id="arells-loader-icon" 
+            src="https://d2d7sp5ao0zph4.cloudfront.net/icons&images/Arells-Icon.png"/>        
+        </div>
+      )}
+      {showLoading && (
+        <div className={styles.spinner}></div>
+      )}
+
+      <div id="overlayy">
+
+        <Image 
+        onLoad={() => handleImageLoaded('arellsIcon')}
+        alt="" 
+        width={80}
+        height={85}
+        id="arells-iconn" 
+        src="https://d2d7sp5ao0zph4.cloudfront.net/icons&images/Arells-Icon.png"/>
+      
+        <br/>
+        
+        <Image
+         onLoad={() => handleImageLoaded('wordLogo')}
+         alt=""
+         width={120}
+         height={40}
+         id="word-logoo" 
+         src="https://d2d7sp5ao0zph4.cloudfront.net/icons&images/Arells-Logo-Ebony.png"/>	
+        
+        <br/>
+        
+        <p id="slogann">ART SELLS</p>
+        
+        <hr id="black-liner"/>
+        
+        <p id="descriptioner">
+          NEVER LOSE MONEY SELLING ART
+        </p>
+        
+        <hr id="black-liner"/>
+        
+        <p id="coming-soonn">COMING SOON</p>
+    
+        {/*<!-- Change below link after test -->*/}
+        <Link legacyBehavior href="/stayupdated" >
+          <a id="updatess">STAY UPDATED</a>
+        </Link>			
+        
+        <div id="prototype-spacer">
+          {/*<!-- Change below link after test -->*/}
+          <Link legacyBehavior href="/prototype-seller-created">
+            <a id="prototype">PROTOTYPE</a>
+          </Link>	        
+        </div>           
+    
+      </div>		       
     </>
   );
 }
 
-export default Home;
+export default Index;
 
-export const metadata = {
-  robots: "noimageindex",
-  httpEquiv: {
-    "X-UA-Compatible": "IE=edge"
-  },
-  charSet: "UTF-8",
-  title: "Arells",
-  description: "Never lose money selling art.",
-  google: "nositelinkssearchbox",
-  keywords: "Arells",
-  author: "Arells",
-  viewport: {
-    content: "width=device-width,user-scalable=yes,initial-scale=1",
-    id: "viewport"
-  },
-  linkCanonical: "https://arells.com",
-  og: {
-    image: "https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
-    site_name: "Arells",
-    type: "website",
-    title: "Arells",
-    url: "https://arells.com",
-    description: "Never lose money selling art.",
-    imageType: "image/jpg",
-    imageWidth: "700",
-    imageHeight: "400"
-  },
-  twitter: {
-    title: "Arells",
-    image: "https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
-    url: "https://arells.com",
-    card: "summary_large_image",
-    description: "Never lose money selling art."
-  }
-};
+
+
+
+// import Index from '../components/Index';
+
+// const Home = () => {
+
+//   return (
+//     <>
+//       <Index/>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+// export const metadata = {
+//   robots: "noimageindex",
+//   httpEquiv: {
+//     "X-UA-Compatible": "IE=edge"
+//   },
+//   charSet: "UTF-8",
+//   title: "Arells",
+//   description: "Never lose money selling art.",
+//   google: "nositelinkssearchbox",
+//   keywords: "Arells",
+//   author: "Arells",
+//   viewport: {
+//     content: "width=device-width,user-scalable=yes,initial-scale=1",
+//     id: "viewport"
+//   },
+//   linkCanonical: "https://arells.com",
+//   og: {
+//     image: "https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
+//     site_name: "Arells",
+//     type: "website",
+//     title: "Arells",
+//     url: "https://arells.com",
+//     description: "Never lose money selling art.",
+//     imageType: "image/jpg",
+//     imageWidth: "700",
+//     imageHeight: "400"
+//   },
+//   twitter: {
+//     title: "Arells",
+//     image: "https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
+//     url: "https://arells.com",
+//     card: "summary_large_image",
+//     description: "Never lose money selling art."
+//   }
+// };
