baea951d16fcea516576a29d80831e23e53a0470
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-08-27 23:52:39 -0700

    Updating Meta Info

--- Apps/Web/arellsdigitalassets/src/app/page.js
+++ Apps/Web/arellsdigitalassets/src/app/page.js
@@ -1,44 +1,11 @@
-import Head from 'next/head'
+
 import Index from '../components/Index';
 
 
 const Home = () => {
 
   return (
-    <>
-      <Head>
-        <meta name="robots" content="noimageindex"/>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-        <meta charSet="UTF-8"/>
-
-        <meta name="title" content="Arells"/>
-        <meta name="description" content="Never lose money selling art."/>
-        <meta name="google" content="nositelinkssearchbox"/>
-        <meta name="keywords" content="Arells"/>
-        <meta name="author" content="Arells"/>
-        <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1"/>
-        {/*<!-- Change below link after test -->*/}
-        <link rel="canonical" href="https://arells.com"/>
-        <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>
-        <meta property="og:site_name" content="Arells"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="Arells"/>
-        {/*<!-- Change below link after test -->*/}
-        <meta property="og:url" content="https://arells.com"/>
-        <meta property="og:description" content="Never lose money selling art."/>
-        <meta property="og:image:type" content="image/jpg"/>
-        <meta property="og:image:width" content="700"/>
-        <meta property="og:image:height" content="400"/>
-
-        <meta name="twitter:title" content="Arells"/>
-        <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg"/>
-        {/*<!-- Change below link after test -->*/}
-        <meta name="twitter:url" content="https://arells.com"/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:description" content="Never lose money selling art."/>
-      </Head>    
-		  <title>Arells</title>	  
-      
+    <>  
       <Index/>
       
     </>
@@ -46,3 +13,39 @@ const Home = () => {
 }
 
 export default Home;
+
+export const metadata = {
+  robots: "noimageindex",
+  httpEquiv: {
+    "X-UA-Compatible": "IE=edge"
+  },
+  charSet: "UTF-8",
+  title: "Arells",
+  description: "Never lose money selling art.",
+  google: "nositelinkssearchbox",
+  keywords: "Arells",
+  author: "Arells",
+  viewport: {
+    content: "width=device-width,user-scalable=yes,initial-scale=1",
+    id: "viewport"
+  },
+  linkCanonical: "https://arells.com",
+  og: {
+    image: "https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
+    site_name: "Arells",
+    type: "website",
+    title: "Arells",
+    url: "https://arells.com",
+    description: "Never lose money selling art.",
+    imageType: "image/jpg",
+    imageWidth: "700",
+    imageHeight: "400"
+  },
+  twitter: {
+    title: "Arells",
+    image: "https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
+    url: "https://arells.com",
+    card: "summary_large_image",
+    description: "Never lose money selling art."
+  }
+};
