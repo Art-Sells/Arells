b07f386e137ad005dc5811e488600a8995d0de38
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-08-28 00:46:19 -0700

    Updated Home

--- Apps/Web/arellsdigitalassets/src/components/Index.js
+++ Apps/Web/arellsdigitalassets/src/components/Index.js
@@ -65,14 +65,15 @@ const Index = () => {
 
   return (
     <>
-          <Head>
-            <meta name="robots" content="noimageindex"/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-            <meta charset="UTF-8"/>
-            <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1" />
-            <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
-            <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
-            </Head>
+    <Head>
+        <meta name="robots" content="noimageindex"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" id="viewport" content="width=device-width,user-scalable=yes,initial-scale=1" />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
+        <meta name="twitter:image" content="https://user-images.githubusercontent.com/51394348/223035337-47c28406-e5f5-4dcd-acb6-f3acd0646646.jpg"/>
+    </Head>
+
       {showLoading && (
         <div id="spinnerBackground">
           <Image 
