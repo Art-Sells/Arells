6ae89c88f4512a04df02fe46f004cd2c5f678104
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-08-31 18:50:59 -0700

    Testing Metatags

--- Apps/Web/arellsdigitalassets/src/app/test/prototype/page.js
+++ Apps/Web/arellsdigitalassets/src/app/test/prototype/page.js
@@ -15,49 +15,49 @@ const SellerCreatedPage = () => {
 
 export default SellerCreatedPage;
 
-export const metadata = {
-    robots: "noimageindex",
-    httpEquiv: {
-      "X-UA-Compatible": "IE=edge"
-    },
-    charSet: "UTF-8",
-    title: "Seller Creations Prototype",
-    description: "Prototype for Seller Creations",
-    google: "nositelinkssearchbox",
-    keywords: ["Arells"],
-    author: "Arells",
-    // Change below link after test  
-    linkCanonical: "https://arells.com/prototype-seller-created",
-    og: {
-        image: [
-            {
-              url:"https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
-              width:700,
-              height:400,
-            }
-          ],
-      site_name: "Arells",
-      type: "website",
-      title: "Seller Creations Prototype",
-      // Change below link after test    
-      url: "https://arells.com/prototype-seller-created",
-      description: "Prototype for Seller Creations",
-      imageType: "image/jpg",
-      imageWidth: "700",
-      imageHeight: "400"
-    },
-    twitter: {
-      title: "Seller Creations Prototype",
-      image: [
-        {
-          url:"https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
-          width:700,
-          height:400,
-        }
-      ],
-      // Change below link after test
-      url: "https://arells.com/prototype-seller-created",
-      card: "summary_large_image",
-      description: "Prototype for Seller Creations"
-    }
-  };
+// export const metadata = {
+//     robots: "noimageindex",
+//     httpEquiv: {
+//       "X-UA-Compatible": "IE=edge"
+//     },
+//     charSet: "UTF-8",
+//     title: "Seller Creations Prototype",
+//     description: "Prototype for Seller Creations",
+//     google: "nositelinkssearchbox",
+//     keywords: ["Arells"],
+//     author: "Arells",
+//     // Change below link after test  
+//     linkCanonical: "https://arells.com/prototype-seller-created",
+//     og: {
+//         image: [
+//             {
+//               url:"https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
+//               width:700,
+//               height:400,
+//             }
+//           ],
+//       site_name: "Arells",
+//       type: "website",
+//       title: "Seller Creations Prototype",
+//       // Change below link after test    
+//       url: "https://arells.com/prototype-seller-created",
+//       description: "Prototype for Seller Creations",
+//       imageType: "image/jpg",
+//       imageWidth: "700",
+//       imageHeight: "400"
+//     },
+//     twitter: {
+//       title: "Seller Creations Prototype",
+//       image: [
+//         {
+//           url:"https://user-images.githubusercontent.com/51394348/227811567-244af8ad-d592-40f9-9188-6d225fffe46f.jpg",
+//           width:700,
+//           height:400,
+//         }
+//       ],
+//       // Change below link after test
+//       url: "https://arells.com/prototype-seller-created",
+//       card: "summary_large_image",
+//       description: "Prototype for Seller Creations"
+//     }
+//   };
