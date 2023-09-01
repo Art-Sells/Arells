539adc033d170d216f877f7226b607e6c078513e
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-08-31 20:24:19 -0700

    Added comment

--- Apps/Web/arellsdigitalassets/next.config.js
+++ Apps/Web/arellsdigitalassets/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
+//For hosted images in AWS (not Metadata images)
 module.exports = {
     images: {
       domains: ['d2d7sp5ao0zph4.cloudfront.net'],
