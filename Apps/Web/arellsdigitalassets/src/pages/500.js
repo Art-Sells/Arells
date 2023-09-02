0865dd951901b34a031c1585993d90388bbe44c9
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-02 15:31:02 -0700

    Added Metadata info to error pages

--- Apps/Web/arellsdigitalassets/src/pages/500.js
+++ Apps/Web/arellsdigitalassets/src/pages/500.js
@@ -11,3 +11,42 @@ export default function Custom500() {
        </>
        );
  }
+
+ export async function getStaticProps() {
+  
+    const metadata = {
+        robots: "noimageindex",
+        httpEquiv: {
+          "X-UA-Compatible": "IE=edge"
+        },
+        charSet: "UTF-8",
+        title: "Page Not Found",
+        description: "This page doesn't exist.",
+        google: "nositelinkssearchbox",
+        keywords: ["Arells"],
+        author: "Arells",
+        //change below link after test
+        linkCanonical: "/",
+        og: {
+          site_name: "Arells",
+          type: "website",
+          title: "Page Not Found",
+      //change below link after test 
+          url: "/",
+          description: "This page doesn't exist.",
+        },
+        twitter: {
+          title: "Page Not Found",
+      // Change below link after test
+          url: "/",
+          card: "summary_large_image",
+          description: "This page doesn't exist.",
+        }
+    };
+  
+    return {
+      props: {
+        metadata
+      }
+    };
+  }
