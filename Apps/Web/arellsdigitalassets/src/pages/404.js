f6bc7be84a05637db1cadc8341965b8ab524d9fd
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-02 15:12:35 -0700

    Testing Custom 404

---
+++ Apps/Web/arellsdigitalassets/src/pages/404.js
@@ -0,0 +1,20 @@
+// import '../app/css/error-style.css';
+
+// import Error from '../components/error/Error';
+
+// const NotFound = () => {
+
+//   return (
+//     <>
+//       <div id="error-overlay">
+//        <Error/>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default NotFound;
+
+export default function Custom404() {
+    return <h1>404 - Page Not Found</h1>
+  }
