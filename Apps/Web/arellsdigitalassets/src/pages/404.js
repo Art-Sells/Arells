3156b78795e623b9edbc761e7e63fc864499d3d3
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-02 15:35:57 -0700

    Updating Metadata info

--- Apps/Web/arellsdigitalassets/src/pages/404.js
+++ Apps/Web/arellsdigitalassets/src/pages/404.js
@@ -12,44 +12,37 @@ export default function Custom404() {
         );
   }
 
-  export async function getStaticProps() {
   
-    const metadata = {
-        robots: "noimageindex",
-        httpEquiv: {
-          "X-UA-Compatible": "IE=edge"
-        },
-        charSet: "UTF-8",
+const metadata = {
+    robots: "noimageindex",
+    httpEquiv: {
+        "X-UA-Compatible": "IE=edge"
+    },
+    charSet: "UTF-8",
+    title: "Page Not Found",
+    description: "This page doesn't exist.",
+    google: "nositelinkssearchbox",
+    keywords: ["Arells"],
+    author: "Arells",
+    //change below link after test
+    linkCanonical: "/",
+    og: {
+        site_name: "Arells",
+        type: "website",
         title: "Page Not Found",
+    //change below link after test 
+        url: "/",
         description: "This page doesn't exist.",
-        google: "nositelinkssearchbox",
-        keywords: ["Arells"],
-        author: "Arells",
-        //change below link after test
-        linkCanonical: "/",
-        og: {
-          site_name: "Arells",
-          type: "website",
-          title: "Page Not Found",
-      //change below link after test 
-          url: "/",
-          description: "This page doesn't exist.",
-        },
-        twitter: {
-          title: "Page Not Found",
-      // Change below link after test
-          url: "/",
-          card: "summary_large_image",
-          description: "This page doesn't exist.",
-        }
-    };
+    },
+    twitter: {
+        title: "Page Not Found",
+    // Change below link after test
+        url: "/",
+        card: "summary_large_image",
+        description: "This page doesn't exist.",
+    }
+};
   
-    return {
-      props: {
-        metadata
-      }
-    };
-  }
   
   
   
