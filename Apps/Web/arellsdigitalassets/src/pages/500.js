e7a9fc62635b21da71645a6ad94817dea128a960
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-02 18:57:33 -0700

    Finishing Touches to Error Pages

--- Apps/Web/arellsdigitalassets/src/pages/500.js
+++ Apps/Web/arellsdigitalassets/src/pages/500.js
@@ -1,16 +1,48 @@
 import '../app/css/error-style.css';
 
-import Head from 'next/head';
 import PageError from '../components/error/500/ServerError';
+
+export async function generateMetadata({}) {
+    let title = "Server Not Found";
+    let description = "The server for this page cannot be found.";
+  
+    let openGraph = {
+      site_name: "Arells",
+      title: title,
+      description: description,
+      // Change this link after testing
+      url: "/", 
+      type: "website",
+      images: [
+        {
+          url: "https://arellsimages.s3.us-west-1.amazonaws.com/icons%26images/metadata-images/twitter-image.jpg"
+        }
+      ]
+    };
+  
+    let twitter = {
+      title: title,
+      description: description,
+      // Change this link after testing
+      url: "/",
+      card: "summary_large_image",
+      images: [
+        {
+          url: "https://arellsimages.s3.us-west-1.amazonaws.com/icons%26images/metadata-images/twitter-image.jpg"
+        }
+      ]
+    };
+  
+    return {
+      robots: "noimageindex",httpEquiv: {"X-UA-Compatible": "IE=edge"},
+      charSet: "UTF-8", linkCanonical: "/", title, description, 
+      openGraph, twitter
+    };
+  }
 
 export default function Custom500() {
      return (
        <>
-            <Head>
-                <title>Server Not Found</title>
-                <meta name="title" content="Server Not Found" />
-                <meta name="description" content="Server for this page not found." />
-            </Head>
            <div id="error-overlay">
                <PageError/>
            </div>
