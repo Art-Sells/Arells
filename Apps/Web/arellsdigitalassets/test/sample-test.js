c1e7242f0339aaec02f3cd2fe74aaf190bb81b31
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-04 18:31:13 -0700

    Installed new packages

--- Apps/Web/arellsdigitalassets/test/sample-test.js
+++ Apps/Web/arellsdigitalassets/test/sample-test.js
@@ -1,7 +1,36 @@
 const {expect} = require("chai");
 
-describe("", function (){
-    it("Should return the new bla bla bla", async function () {
+describe("NFTMarket", function (){
+    it("Should create and execute market sales", async function () {
+        const Market = await ethers.getContractFactory("NFTMarket")
+        const market = await Market.deploy()
+        await market.deployed()
+        const marketAddress = market.address
+
+        const NFT = await ethers.getContractFactory("NFT")
+        const nft = await NFT.deploy(marketAddress)
+        await nft.deployed()
+        const nftContractAddres = nft.address
+
+        let listingPrice = await market.getListingPrice()
+        listingPrice = listingPrice.toString()
+
+        const price = ethers.utils.parseUnits('100', 'ether')
+
+        await nft.createToken("https://mytokenlocation.com")
+        await nft.createToken("https://mytokenlocation2.com")
+
+        await market.createMarketItem(nftContractAddres, 1, price, {value: listingPrice})
+        await market.createMarketItem(nftContractAddres, 2, price, {value: listingPrice})
+
+        const [_, buyerAddress] = await ethers.getSigners()
+
+        await market.connect(buyerAddress).createMarketSale(nftContractAddres, 1, {value:
+        price})
+
+        const items = await market.fetchMarketItems()
+
+        console.log('items: ', items)
 
     });
 });
