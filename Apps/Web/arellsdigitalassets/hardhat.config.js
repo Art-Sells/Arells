7dc7c5aca25d84201f42b765d6c3cfb1a6566db5
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-08 20:17:53 -0700

    Try again...

--- Apps/Web/arellsdigitalassets/hardhat.config.js
+++ Apps/Web/arellsdigitalassets/hardhat.config.js
@@ -1,24 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ethers");
-require("dotenv").config();
+// require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-ethers");
+// require("dotenv").config();
 
-const fs = require("fs")
-const privateKey = fs.readFileSync(".secret").toString()
-const projectId = "2b4efb18d4df4884bf0cd54db5719d7f"
+// const fs = require("fs")
+// const privateKey = fs.readFileSync(".secret").toString()
+// const projectId = "2b4efb18d4df4884bf0cd54db5719d7f"
 
-module.exports = {
-  networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: [privateKey]
-    },
-    mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-      accounts: [privateKey]
-    }
-  },
-  solidity: "0.8.18",
-};
+// module.exports = {
+//   networks: {
+//     hardhat: {
+//       chainId: 1337
+//     },
+//     mumbai: {
+//       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+//       accounts: [privateKey]
+//     },
+//     mainnet: {
+//       url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+//       accounts: [privateKey]
+//     }
+//   },
+//   solidity: "0.8.18",
+// };
