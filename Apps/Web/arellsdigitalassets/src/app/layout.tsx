ca2da84e16d8c907cf927dd6b5f52c027c4379fe
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-09-26 11:14:38 -0700

    new asset store added

---
+++ Apps/Web/arellsdigitalassets/src/app/layout.tsx
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
