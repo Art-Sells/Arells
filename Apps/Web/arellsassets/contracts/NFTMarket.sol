6453f2ccde05e21dcc1f652f4216f8405a1b0b1f
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-10-03 20:56:35 -0700

    Till Tomorrow

--- Apps/Web/arellsassets/contracts/NFTMarket.sol
+++ Apps/Web/arellsassets/contracts/NFTMarket.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "../node_modules";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
