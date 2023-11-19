d0b3ff0cc89171d8b340833e58118f3739e7813a
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-11-18 20:21:54 -0800

    Testing visuals Complete

--- Apps/Web/arellsstore/src/state/nft-market/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export type NFT = {
-    id: string;
-    // Owner of NFT, if NFT is listed for sale, this will be the seller address
-    owner: string;
-    storeAddress: any;
-    // If price > 0, the NFT is for sale
-    price: string;
-    tokenURI: string;
-  };
