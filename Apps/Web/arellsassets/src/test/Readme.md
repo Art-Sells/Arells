7e2675789aaf77eb24c6cad1c7985b7a1f875ca3
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-12-28 21:44:57 -0800

    Refactored Branch

---
+++ Apps/Web/arellsassets/src/test/Readme.md
@@ -0,0 +1,1 @@
+Move this Test Folder into "App" To Build & Test.
---
+++ Apps/Web/arellsstore/src/app/create/page.tsx
@@ -0,0 +1,47 @@
+import '../css/prototype/asset/asset.css';
+
+import React from "react";
+import CreateArt from '../../components/Create/CreateArt';
+
+import type { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: "Create Art",
+  description: "Create art that never loses value.",
+  robots: "noimageindex",
+  openGraph: {
+    title: "Create art that never loses value.",
+    description: "Render bear markets obsolete with Arells.",
+    url: "https://arells.com/create",
+    type: "website",
+    images: [
+      {
+        url: "https://arellsimages.s3.us-west-1.amazonaws.com/icons&images/metadata-images/Default-Spread.jpg"
+      }
+    ]
+  },
+  twitter: {
+    title: "Create art that never loses value.",
+    description: "Render bear markets obsolete with Arells.",
+    card: "summary_large_image",
+    images: [
+      {
+        url: "https://arellsimages.s3.us-west-1.amazonaws.com/icons&images/metadata-images/Default-Spread.jpg"
+      }
+    ]
+  }
+};
+
+const CreateTest = () => {
+
+  return (
+    <>
+        <div id="asset-wrapper">
+          <CreateArt/>
+        </div>  
+    </>
+  );
+}
+
+export default CreateTest;
+
+
