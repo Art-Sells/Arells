bddeba65a7439277089c1a1ac908432cb71c36be
Jeremy Akatsa <51394348+jeyakatsa@users.noreply.github.com>
2024-04-24 20:00:48 -0700

    Update Index.tsx

--- Apps/Web/arellsstore/src/components/Index.tsx
+++ Apps/Web/arellsstore/src/components/Index.tsx
@@ -120,7 +120,7 @@ const Index = () => {
       {openStore && (
         <div>          
           <p id="descriptioner">
-          BUY ASSETS THAT NEVER LOSE VALUE
+          SELL ITEMS THAT NEVER LOSE VALUE
           </p>
 
           <hr id="black-liner"/>
