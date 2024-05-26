8c35effec16adad2d07f0013ae0cab08c9b6bc3b
Jeremy Akatsa <jeyakatsa@gmail.com>
2024-05-25 22:37:12 -0700

    More Touchups

--- Apps/Web/arellsstore/src/components/HowTo/HowItWorks.tsx
+++ Apps/Web/arellsstore/src/components/HowTo/HowItWorks.tsx
@@ -8,8 +8,8 @@ import '../../app/css/modals/stayupdated-modal.css';
 
 
 // Loader Styles
-import '../../app/css/modals/loading/spinnerBackground.css';
-import styles from '../../app/css/modals/loading/spinner.module.css';
+import '../../app/css/modals/loading/spinnerBackgroundHow.css';
+import styles from '../../app/css/modals/loading/howspinner.module.css';
 
 const HowItWorks: React.FC = () => {
     //Loader Function/s
