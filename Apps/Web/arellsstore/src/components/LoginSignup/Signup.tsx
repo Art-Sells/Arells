eaed5d5b33ec0412c462d00577814c5889fe95c4
Jeremy Akatsa <jeyakatsa@gmail.com>
2024-06-15 00:01:21 -0700

    Until tomorrow

--- Apps/Web/arellsstore/src/components/LoginSignup/Signup.tsx
+++ Apps/Web/arellsstore/src/components/LoginSignup/Signup.tsx
@@ -7,8 +7,10 @@ import '../app/css/loginsignup/loginsignup.css';
 import '../app/css/modals/loginsignup/loginsignup-modal.css';
 import $ from 'jquery';
 import Link from 'next/link';
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from '../../aws-exports';
+import Amplify from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from '../../aws-exports'; // Ensure this path is correct
 
 Amplify.configure(awsconfig);
 
@@ -18,124 +20,102 @@ const Signup: React.FC = () => {
         return `/${src}?w=${width}&q=${quality || 100}`;
     };
 
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [confirmPassword, setConfirmPassword] = useState<string>('');
     const [showEnterInformation, setEnterInformation] = useState<boolean>(false);
     const [showSubmitted, setSubmitted] = useState<boolean>(false);
-
-    const signUp = async () => {
-        if (password !== confirmPassword) {
-            alert('Passwords do not match');
-            return;
-        }
-
-        try {
-            await Auth.signUp({
-                username: email,
-                password,
-                attributes: {
-                    email
-                }
-            });
-            setSubmitted(true);
-        } catch (error) {
-            console.log('Error signing up:', error);
-        }
-    };
 
     const closeEnterInformation = () => {
         setEnterInformation(false);
     };
 
     return (
-        <>
-            {showEnterInformation && (
-                <div className="RWmodal">
-                    <div className="RWmodal-content">
-                        <p className="enter-info">ENTER INFORMATION</p>
-                        <button className="RWclose" onClick={closeEnterInformation}>OK</button>
-                    </div>
-                </div>
-            )}
-
-            {showSubmitted && (
-                <div className="RWmodal-submitted">
-                    <div className="RWmodal-content-submitted">
-                        <p className="submission-successful">SUBMITTED</p>
-                        <div className="contact-submit">
-                            <p className="contact-submit-question">Questions? Contact us:</p>
-
-                            <a href="mailto:info@arells.com"
-                                className="email-contacts" >
-                                <Image
-                                    loader={imageLoader}
-                                    alt=""
-                                    width={25}
-                                    height={25}
-                                    id="email-contact"
-                                    src="images/signup/email-ebony.png" />
-                            </a>
-
-                            <Link href="https://twitter.com/arellsofficial"
-                                passHref
-                                className="twitter-contacts">
-                                <Image
-                                    loader={imageLoader}
-                                    alt=""
-                                    width={25}
-                                    height={25}
-                                    id="twitter-contact"
-                                    src="images/signup/twitter-ebony.png" />
-                            </Link>
-
+        <Authenticator>
+            {({ signOut, user }) => (
+                <>
+                    {showEnterInformation && (
+                        <div className="RWmodal">
+                            <div className="RWmodal-content">
+                                <p className="enter-info">ENTER INFORMATION</p>
+                                <button className="RWclose" onClick={closeEnterInformation}>OK</button>
+                            </div>
                         </div>
-                        <p className="contact-title-description">
-                            NEVER LOSE MONEY SELLING CRYPTOCURRENCIES
-                        </p>
-                        <p className="contact-coming-soon">
-                            COMING SOON
-                        </p>
-                    </div>
-                </div>
-            )}
+                    )}
 
-            <p id="stay-updated">SIGN UP</p>
+                    {showSubmitted && (
+                        <div className="RWmodal-submitted">
+                            <div className="RWmodal-content-submitted">
+                                <p className="submission-successful">SUBMITTED</p>
+                                <div className="contact-submit">
+                                    <p className="contact-submit-question">Questions? Contact us:</p>
 
-            <br />
+                                    <a href="mailto:info@arells.com"
+                                        className="email-contacts" >
+                                        <Image
+                                            loader={imageLoader}
+                                            alt=""
+                                            width={25}
+                                            height={25}
+                                            id="email-contact"
+                                            src="images/signup/email-ebony.png" />
+                                    </a>
 
-            <div id="sign-up">
-                <form id="myForm">
-                    <div id="enter-content">
-                        <label id="label">Email</label>
-                        <br />
-                        <input name="email" type="email"
-                            id="email-input"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div id="enter-content">
-                        <label id="label">Password</label>
-                        <br />
-                        <input name="password" type="password"
-                            id="password-input"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <div id="enter-content">
-                        <label id="label">Confirm Password</label>
-                        <br />
-                        <input name="confirm_password" type="password"
-                            id="confirm-password-input"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)} />
-                    </div>
+                                    <Link href="https://twitter.com/arellsofficial"
+                                        passHref
+                                        className="twitter-contacts">
+                                        <Image
+                                            loader={imageLoader}
+                                            alt=""
+                                            width={25}
+                                            height={25}
+                                            id="twitter-contact"
+                                            src="images/signup/twitter-ebony.png" />
+                                    </Link>
+
+                                </div>
+                                <p className="contact-title-description">
+                                    NEVER LOSE MONEY SELLING CRYPTOCURRENCIES
+                                </p>
+                                <p className="contact-coming-soon">
+                                    COMING SOON
+                                </p>
+                            </div>
+                        </div>
+                    )}
+
+                    <p id="stay-updated">SIGN UP</p>
+
                     <br />
-                    <a id="submit"
-                        onClick={signUp}>SIGN UP</a>
-                </form>
-            </div>
-        </>
+
+                    <div id="sign-up">
+                        <form id="myForm">
+                            <div id="enter-content">
+                                <label id="label">Email</label>
+                                <br />
+                                <input name="email" type="email"
+                                    id="email-input"
+                                    />
+                            </div>
+                            <div id="enter-content">
+                                <label id="label">Password</label>
+                                <br />
+                                <input name="password" type="password"
+                                    id="password-input"
+                                    />
+                            </div>
+                            <div id="enter-content">
+                                <label id="label">Confirm Password</label>
+                                <br />
+                                <input name="confirm_password" type="password"
+                                    id="confirm-password-input"
+                                    />
+                            </div>
+                            <br />
+                            <a id="submit"
+                                onClick={() => setSubmitted(true)}>SIGN UP</a>
+                        </form>
+                    </div>
+                </>
+            )}
+        </Authenticator>
     );
 }
 
