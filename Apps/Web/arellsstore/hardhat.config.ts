98c2ffba8be9b206de26a65e99c73ebc34161f38
Jeremy Akatsa <51394348+jeyakatsa@users.noreply.github.com>
2025-06-14 10:30:23 -0700

    Update hardhat.config.ts

--- Apps/Web/arellsstore/hardhat.config.ts
+++ Apps/Web/arellsstore/hardhat.config.ts
@@ -3,5 +3,22 @@ import 'dotenv/config';
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 
+ const privateKey = process.env.ARELLS_PRIVATE_KEY || "";
+ const projectId: string = "4885ed01637e4a6f91c2c7fcd1714f68";
 
-  // export default config;
+ const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      { version: "0.8.20" }, // For MASSTester.sol
+      { version: "0.7.6" },  // For Uniswap V3 libraries
+    ],
+  },
+  networks: {
+    base: {
+      url: `https://base-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+};
+
+export default config;
