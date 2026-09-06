f1d7bc196d9f1a470a39a5e887e5f78346a273dd
Jeremy Akatsa <jeyakatsa@gmail.com>
2026-09-06 00:22:01 -0700

    .

--- Apps/Web/arellsstore/src/lib/bitcoinAlienRaceThemeSongs.ts
+++ Apps/Web/arellsstore/src/lib/bitcoinAlienRaceThemeSongs.ts
@@ -9,12 +9,12 @@ const THEME_SONGS_BASE = '/images/banners/assets/crypto/Bitcoin/ThemeSongs';
 export const ALIEN_RACE_THEME_SONGS: AlienRaceThemeSong[] = [
   {
     id: 'alien-race',
-    title: 'The Bitcoin Alien Race (song)',
+    title: '♫ The Bitcoin Alien Race',
     src: `${THEME_SONGS_BASE}/TheBitcoinAlienRaceTheme.mp3`,
   },
   {
     id: 'daughters-father',
-    title: "The Daughter's Father (song)",
+    title: "♫ The Daughter's Father",
     src: `${THEME_SONGS_BASE}/TheDaughtersFatherTheme.mp3`,
   },
 ];
